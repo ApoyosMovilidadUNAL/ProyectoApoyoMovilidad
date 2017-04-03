0d89f98b0cd5473fa182be228ae50e28d0724fc2
jmhurtadoc@unal.edu.co <jmhurtadoc@unal.edu.co>
2017-04-03 15:04:48 -0500

    Reiniciando modelo de datos con Omar y Jesus

--- db/schema.rb
+++ db/schema.rb
@@ -10,62 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323044349) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "documentos", force: :cascade do |t|
-    t.string   "DOCU_NOMBRE", null: false
-    t.string   "DOCU_RUTA",   null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "estudiantes", force: :cascade do |t|
-    t.string   "ESTU_NOMBRE",   null: false
-    t.string   "ESTU_PASS",     null: false
-    t.string   "ESTU_EMAIL",    null: false
-    t.integer  "ESTU_ROL"
-    t.integer  "ESTU_CEDULA",   null: false
-    t.string   "ESTU_FACULTAD", null: false
-    t.string   "ESTU_CARRERA",  null: false
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  create_table "profesores", force: :cascade do |t|
-    t.string   "PROFE_NOMBRE", null: false
-    t.string   "PROFE_PASS",   null: false
-    t.string   "PROFE_EMAIL",  null: false
-    t.integer  "PROFE_ROL"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  create_table "solicitudes", force: :cascade do |t|
-    t.integer  "SOLI_ESTADO"
-    t.integer  "documentos_id"
-    t.integer  "soportes_id"
-    t.integer  "estudiantes_id"
-    t.integer  "profesores_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.index ["documentos_id"], name: "index_solicitudes_on_documentos_id", using: :btree
-    t.index ["estudiantes_id"], name: "index_solicitudes_on_estudiantes_id", using: :btree
-    t.index ["profesores_id"], name: "index_solicitudes_on_profesores_id", using: :btree
-    t.index ["soportes_id"], name: "index_solicitudes_on_soportes_id", using: :btree
-  end
-
-  create_table "soportes", force: :cascade do |t|
-    t.string   "SOPOR_NOMBRE", null: false
-    t.string   "SOPOR_RUTA",   null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  add_foreign_key "solicitudes", "documentos", column: "documentos_id"
-  add_foreign_key "solicitudes", "estudiantes", column: "estudiantes_id"
-  add_foreign_key "solicitudes", "profesores", column: "profesores_id"
-  add_foreign_key "solicitudes", "soportes", column: "soportes_id"
 end
--- test/models/solicitude_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SolicitudeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
