872af4862ea2af7a016620a63e3bcb5fda4cb620
Jesus Monroy <jdmonroyg@unal.edu.co>
2017-04-11 18:57:00 -0500

    Diferencia homepage admin/student Trabajo con Johan

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-  	@user_info = Student.find_by(stu_email: current_user.email)
+  	@student = Student.find_by(stu_email: current_user.email)
   end
 end
