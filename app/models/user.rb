087f890af634bd09acb46751e9374b9b70f00fb3
Johan_Sanabria <djsanabriac@unal.edu.co>
2017-04-28 14:51:47 -0500

    LDAP Funcionando

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   	# :confirmable, :lockable, :timeoutable and :omniauthable
   	devise :ldap_authenticatable, :rememberable, :trackable, :validatable,
   	:authentication_keys => [:username]
+  	after_create :save_student
+  	after_update :save_student
    	#has_one :student
 	#accepts_nested_attributes_for :student
 
@@ -14,5 +16,15 @@ class User < ApplicationRecord
 		self.email = Devise::LDAP::Adapter.get_ldap_param(self.username,"mail").first
 		#self.name = Devise::LDAP::Adapter.get_ldap_param(self.username,"givenname").first
 		#self.lastname = Devise::LDAP::Adapter.get_ldap_param(self.username,"sn").first
+	end
+	def save_student
+		student =  Student.new(
+		  :stu_name =>  username,
+		  :stu_email =>  username + "@unal.edu.co",
+		  :stu_rol => 2,
+		  :stu_identification =>  300,
+		  :stu_facultad =>  'ingenieria',
+		  :stu_carrera => 'ingenieria de sistemas')
+		student.save!
 	end
 end
