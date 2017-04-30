e3b4b3237cd5b01a65e0ef5b62237af42cbde28b
jmhurtadoc@unal.edu.co <jmhurtadoc@unal.edu.co>
2017-04-30 14:22:42 -0500

    Se agregan css y js unal Jesus Johan Jhader

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,21 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( css/base.css )
+Rails.application.config.assets.precompile += %w( css/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( css/phone.css )
+Rails.application.config.assets.precompile += %w( css/printer.css )
+Rails.application.config.assets.precompile += %w( css/reset.css )
+Rails.application.config.assets.precompile += %w( css/small.css )
+Rails.application.config.assets.precompile += %w( css/tablet.css )
+Rails.application.config.assets.precompile += %w( css/unal.css )
 
+Rails.application.config.assets.precompile += %w( js/html5dhiv.js )
+Rails.application.config.assets.precompile += %w( js/jquery.js )
+Rails.application.config.assets.precompile += %w( js/matchmedia.addListener.js )
+Rails.application.config.assets.precompile += %w( js/matchmedia.polyfill.js )
+Rails.application.config.assets.precompile += %w( js/respond.js )
+Rails.application.config.assets.precompile += %w( js/unal.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
